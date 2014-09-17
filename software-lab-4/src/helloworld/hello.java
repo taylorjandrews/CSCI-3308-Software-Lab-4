d8d117e9b477ff14aae358f537e09ae5863325b2
Taylor Andrews <andrewstaylorj@gmail.com>
2014-09-17 15:25:01 -0600

    Added "I am well."

--- software-lab-4/src/helloworld/hello.java
+++ software-lab-4/src/helloworld/hello.java
@@ -4,5 +4,6 @@ public class hello {
 	public static void main(String[] args) {
 		System.out.println("Hello world!"); // Display the string.
 		System.out.println("How are you?");
+		System.out.println("I am well.");
 	}
 }
